db1c8962f0e172a7f62fa64645394172219f03ea
maybe2009 <ox07c00@163.com>
2018-03-12 07:47:32 +0800

    add net src&header file

---
+++ src/net/net.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tombkeeper on 2018/3/11.
+//
+
+#include "net.h"
---
+++ src/net/net.h
@@ -0,0 +1,21 @@
+//
+// Created by tombkeeper on 2018/3/11.
+//
+
+#ifndef EPOLL_NET_H
+#define EPOLL_NET_H
+
+#include <string>
+
+namespace net {
+    class Conn {
+
+    };
+
+    Conn* Dial(const std::string& address) {
+
+    }
+};
+
+
+#endif //EPOLL_NET_H
